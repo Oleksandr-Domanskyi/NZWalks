dbc7fc39ecab286f9a12c13f2aaf84a77239919c
Oleksandr Domanskyi <sasha2001do@gmail.com>
2023-12-14 08:55:13 +0100

    PUT and DELETE NZWalks.UI

--- NZWalks.UI/Controllers/RegionsController.cs
+++ NZWalks.UI/Controllers/RegionsController.cs
@@ -68,5 +68,64 @@ namespace NZWalks.UI.Controllers
             }
             return View();
         }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var client = httpClientFactory.CreateClient();
+            var response = await client.GetFromJsonAsync<RegionDto>($"http://localhost:5266/api/Region/{id.ToString()}");
+            if (response is not null)
+            {
+                return View(response);
+            }
+
+
+            return View(null);
+        }
+        [HttpPost]
+        public async Task<IActionResult>Edit(RegionDto request)
+        {
+            var client = httpClientFactory.CreateClient();
+
+            var httprequestMessage = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Put,
+                RequestUri = new Uri($"http://localhost:5266/api/Region/{request.Id}"),
+                Content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json")
+            };
+
+           var httpResponseMessage = await client.SendAsync(httprequestMessage);
+           httpResponseMessage.EnsureSuccessStatusCode();
+
+            var response = await httpResponseMessage.Content.ReadFromJsonAsync<RegionDto>();
+
+            if (response != null)
+            {
+                return RedirectToAction("Edit", "Regions");
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(RegionDto request)
+        {
+            try
+            {
+                var client = httpClientFactory.CreateClient();
+
+                var httpResponseMessage = await client.DeleteAsync($"http://localhost:5266/api/Region/{request.Id}");
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                return RedirectToAction("Index", "Regions");
+            }
+            catch (Exception ex)
+            {
+
+                
+            }
+            return View("");
+        }
+
     }
 }
